b6495371bc60b85644e96afcbdeefd995f780339
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-04 16:39:18 +0530

    creating an array with object ad using that object in Components

--- src/App.js
+++ src/App.js
@@ -4,32 +4,56 @@ import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
 
-  let expenseDate = new Date(2021 , 3, 28);
-  let expenseTitle = "School Fee";
-  let expenseAmout = 300;
+  let expense = [
+    {
+      id : 1,
+      date  : new Date(2021 , 1, 1),
+      title : "School Fee",
+      amout : 9000
+    },
+    {
+      id : 1,
+      date  : new Date(2021 , 10, 4),
+      title : "Liberary Fee",
+      amout : 1500
+    },
+    {
+      id : 1,
+      date  : new Date(2021 , 9, 8),
+      title : "Insurence",
+      amout : 2300
+    },
+    {
+      id : 1,
+      date  : new Date(2021 , 3, 28),
+      title : "Hostel Rent",
+      amout : 5300
+    }
+  ]
+
 
   return (
     <div>
         <h1>Hello!</h1>
         <ExpenseItem 
-            date={expenseDate} 
-            title={expenseTitle} 
-            amount={expenseAmout} >
+            date={expense[0].date} 
+            title={expense[0].title} 
+            amount={expense[0].amout} >
         </ExpenseItem>
         <ExpenseItem 
-            date={expenseDate} 
-            title={expenseTitle} 
-            amount={expenseAmout} >
+            date={expense[1].date} 
+            title={expense[1].title} 
+            amount={expense[1].amout} >
         </ExpenseItem>
         <ExpenseItem 
-            date={expenseDate} 
-            title={expenseTitle} 
-            amount={expenseAmout} >
+            date={expense[2].date} 
+            title={expense[2].title} 
+            amount={expense[2].amout} >
         </ExpenseItem>
         <ExpenseItem 
-            date={expenseDate} 
-            title={expenseTitle} 
-            amount={expenseAmout} >
+            date={expense[3].date} 
+            title={expense[3].title} 
+            amount={expense[3].amout} >
         </ExpenseItem>
 
     </div>
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -16,7 +16,7 @@ function ExpenseItem(props){
                   <div className="expense-item__description">
                         <h2>{ props.title}</h2>
                   </div>
-                  <div className="expense-item__price">${ props.amount}</div>
+                  <div className="expense-item__price">$ { props.amount}</div>
             </div>
       );
 };
