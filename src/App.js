36437951dea16cc886a67a63f9476a7de83008aa
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-04 16:25:49 +0530

    First Comit , starting of Expence-Tracker Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+
+  let expenseDate = new Date(2021 , 3, 28);
+  let expenseTitle = "School Fee";
+  let expenseAmout = 300;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Hello!</h1>
+        <ExpenseItem 
+            date={expenseDate} 
+            title={expenseTitle} 
+            amount={expenseAmout} 
+            />
+
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props){
+
+      const month = props.date.toLocaleString('en-US' , {month: "long"});
+      const year = props.date.getFullYear();
+      const day = props.date.toLocaleString('en-US' , {day:'2-digit'});
+
+      return(
+            <div className="expense-item">
+                  <div>
+                        <div>{month}</div>
+                        <div>{year}</div>
+                        <div>{day}</div>
+                  </div>
+                  <div className="expense-item__description">
+                        <h2>{ props.title}</h2>
+                  </div>
+                  <div className="expense-item__price">${ props.amount}</div>
+            </div>
+      );
+};
+
+export default ExpenseItem;
