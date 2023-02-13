eb9d60a1c294b5b7de224553754b50a10940ef9a
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-13 14:12:06 +0530

    Changing the title value by dynamically using input

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React,{ useState } from 'react';
+// import { useState } from 'react';
 import "./ExpenseDate.css";
 
 import ExpenseDate from "./ExpenseDate";
@@ -9,13 +9,25 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+      const [newTitle, setNewTitle] = useState("");
+
+      const [title, setTitle] = useState(props.title);
+
+      const clickHandler = () =>{
+            setTitle(newTitle);
+      }
+
+      const changeHandler = (event) =>{
+            setNewTitle(event.target.value);
+      }
+
       return(
             <Card className="expense-item">
                   <ExpenseDate date={props.date}/>
-                  <div className="expense-item__description">
-                        <h2>{ props.title}</h2>
-                  </div>
-                  <div className="expense-item__price">$ { props.amount}</div>
+                  <div className="expense-item__description"><h2>{ title }</h2></div>
+                  <div className="expense-item__price">${ props.amount}</div>
+                  <input type="text" value={newTitle} onChange={changeHandler}></input>
+                  <button onClick={clickHandler}>Click</button>
             </Card>
       );
 };
